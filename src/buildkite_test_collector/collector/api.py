32bb405d687a819db637191f552da065da193372
James Harton <59449+jimsynz@users.noreply.github.com>
2022-09-07 18:55:21 +1200

    improvement(debug): emit warning about a missing API token when debug is enabled. (#9)
    Closes #7.

--- src/buildkite_test_collector/collector/api.py
+++ src/buildkite_test_collector/collector/api.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 from os import environ
+from sys import stderr
 from requests import post, Response
 from .payload import Payload
 
@@ -9,7 +10,11 @@ from .payload import Payload
 def submit(payload: Payload, batch_size=100) -> Optional[Response]:
     """Submit a payload to the API"""
     token = environ.get("BUILDKITE_ANALYTICS_TOKEN")
+    debug = environ.get("BUILDKITE_ANALYTICS_DEBUG_ENABLED")
     response = None
+
+    if debug and not token:
+        print("Warning: No `BUILDKITE_ANALYTICS_TOKEN` environment variable present", file=stderr)
 
     if token:
         for payload_slice in payload.into_batches(batch_size):
@@ -18,7 +23,8 @@ def submit(payload: Payload, batch_size=100) -> Optional[Response]:
                             headers={
                                 "Content-Type": "application/json",
                                 "Authorization": f"Token token=\"{token}\""
-                            })
+                            },
+                            timeout=30)
             if response.status_code >= 300:
                 return response
 

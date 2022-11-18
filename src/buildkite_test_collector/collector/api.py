386ad089ec29f493ba25610d001ab1468a19f5e4
Steven Webb <steve@buildkite.com>
2022-11-19 01:39:38 +0800

    Gracefully handle return characters in api token (#12)

--- src/buildkite_test_collector/collector/api.py
+++ src/buildkite_test_collector/collector/api.py
@@ -4,6 +4,7 @@ from typing import Optional
 from os import environ
 from sys import stderr
 from requests import post, Response
+from requests.exceptions import InvalidHeader
 from .payload import Payload
 
 
@@ -17,15 +18,19 @@ def submit(payload: Payload, batch_size=100) -> Optional[Response]:
         print("Warning: No `BUILDKITE_ANALYTICS_TOKEN` environment variable present", file=stderr)
 
     if token:
-        for payload_slice in payload.into_batches(batch_size):
-            response = post("https://analytics-api.buildkite.com/v1/uploads",
-                            json=payload_slice.as_json(),
-                            headers={
-                                "Content-Type": "application/json",
-                                "Authorization": f"Token token=\"{token}\""
-                            },
-                            timeout=30)
-            if response.status_code >= 300:
-                return response
+        try:
+            for payload_slice in payload.into_batches(batch_size):
+                response = post("https://analytics-api.buildkite.com/v1/uploads",
+                                json=payload_slice.as_json(),
+                                headers={
+                                    "Content-Type": "application/json",
+                                    "Authorization": f"Token token=\"{token}\""
+                                },
+                                timeout=30)
+                if response.status_code >= 300:
+                    return response
+        except InvalidHeader as error:
+            print("Warning: Invalid `BUILDKITE_ANALYTICS_TOKEN` environment variable", file=stderr)
+            print(error, file=stderr)
 
     return response

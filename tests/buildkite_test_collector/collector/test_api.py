c0c8d8fe8a66e824a0865415cbcf52ab31c00253
Jordan Sanders <jordan@dagsterlabs.com>
2025-05-05 08:53:00 -0500

    Assert later batches submit if earlier ones fail

--- tests/buildkite_test_collector/collector/test_api.py
+++ tests/buildkite_test_collector/collector/test_api.py
@@ -155,6 +155,47 @@ def test_submit_with_large_payload_batches_requests(successful_test, failed_test
             assert json['queued'] == 1
 
 @responses.activate
+def test_submit_with_batches_and_errors(capfd, successful_test, failed_test):
+    responses.add(
+        responses.POST,
+        "https://analytics-api.buildkite.com/v1/uploads",
+        body=ConnectTimeout("Error"))
+    responses.add(
+        responses.POST,
+        "https://analytics-api.buildkite.com/v1/uploads",
+        json={'id': str(uuid4()),
+              'run_id': str(uuid4()),
+              'queued': 1,
+              'skipped': 0,
+              'errors': [],
+              'run_url': 'https://buildkite.com/organizations/alembic/analytics/suites/test/runs/52c5d9f6-a4f2-4a2d-a1e6-993335789c92'},
+        status=202)
+
+    with mock.patch.dict(os.environ, {"CI": "true", "BUILDKITE_ANALYTICS_TOKEN": str(uuid4())}):
+        payload = Payload.init(detect_env())
+        payload = Payload.started(payload)
+
+        payload = payload.push_test_data(successful_test)
+        payload = payload.push_test_data(failed_test)
+
+        results = [response for response in submit(payload, batch_size=1)]
+        assert len(results) == 2
+
+        assert not results[0]
+        captured = capfd.readouterr()
+        assert captured.err.startswith("buildkite-test-collector - WARNING -")
+        assert "ConnectTimeout" in captured.err
+
+        result = results[1]
+        assert result
+        assert result.status_code >= 200
+        assert result.status_code < 300
+
+        json = result.json()
+        assert len(json["errors"]) == 0
+        assert json['queued'] == 1
+
+@responses.activate
 def test_api_url_override(successful_test):
     upload_id = str(uuid4())
 
